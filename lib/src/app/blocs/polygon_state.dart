86525281ab0d0a16f1516d0483fa95672d66d590
izasoerya <ihza.surya@gmail.com>
2024-10-30 15:27:14 +0700

    implement bloc as logic business polygon

--- lib/src/app/blocs/polygon_state.dart
+++ lib/src/app/blocs/polygon_state.dart
@@ -1,12 +1,16 @@
 import 'package:bamboo_app/src/domain/entities/e_polygon.dart';
+import 'package:bamboo_app/src/domain/infrastructure/i_polygon.dart';
+import 'package:bamboo_app/utils/default_user.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 abstract class PolygonEvent {}
 
 class TogglePolygonEvent extends PolygonEvent {}
 
+class FetchPolygonData extends PolygonEvent {}
+
 class PolygonState {
-  final List<EntitiesPolygon> polygons;
+  final List<EntitiesPolygon?> polygons;
 
   PolygonState({required this.polygons});
 }
@@ -15,6 +19,11 @@ class PolygonStateBloc extends Bloc<PolygonEvent, PolygonState> {
   PolygonStateBloc() : super(PolygonState(polygons: [])) {
     on<TogglePolygonEvent>((event, emit) {
       emit(PolygonState(polygons: state.polygons));
+    });
+    on<FetchPolygonData>((event, emit) async {
+      final polygons =
+          await InfrastructurePolygon().readPolygons(defaultUser.uid);
+      emit(PolygonState(polygons: polygons));
     });
   }
 }
--- lib/src/app/presentation/layout/app_layout.dart
+++ lib/src/app/presentation/layout/app_layout.dart
@@ -1,4 +1,6 @@
+import 'package:bamboo_app/src/app/blocs/polygon_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppLayout extends StatelessWidget {
   final Widget child;
@@ -7,7 +9,10 @@ class AppLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: child),
+      body: BlocProvider(
+        create: (context) => PolygonStateBloc(),
+        child: SafeArea(child: child),
+      ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 50,
